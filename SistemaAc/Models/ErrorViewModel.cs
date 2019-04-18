049b2ee029905f7049bddfea3917a9edeb100370
Pc <coli@tobynson.com>
2019-04-18 09:34:51 -0500

    se agrego proyecto sistemaAC y migracion

---
+++ SistemaAc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaAc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
