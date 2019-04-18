387581cd952f7a8050bfe47293be855373b43133
Pc <coli@tobynson.com>
2019-04-18 15:16:20 -0500

    se agrego identiy pages

---
+++ SistemaAc/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaAc.Data;
+
+[assembly: HostingStartup(typeof(SistemaAc.Areas.Identity.IdentityHostingStartup))]
+namespace SistemaAc.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
